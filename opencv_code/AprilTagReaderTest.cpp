e326532ad3cea2e2e73fec4cefca44aeb3a8ad03
Nathan Piasco <nathan.piasco@gmail.com>
2018-01-16 12:06:43 +0100

    Original commit

---
+++ opencv_code/AprilTagReaderTest.cpp
@@ -0,0 +1,15 @@
+#include "AprilTagReader.h"
+
+
+
+
+int main()
+{
+
+    // Fonction cv::imshow("NomDeLaFenetre", image) pour afficher une image dans une fenetre
+
+    // cv::waitKey(1) permet d'afficher les fenetres. Renvoie une valeur négative si on essaye de quitter la fenetre
+
+
+    return 0;
+}
